ffbcca7bfc1d813973ca610b6586281e937d5076
Souza1981 <souza1981@gmail.com>
2020-06-10 21:33:17 -0300

    Correção dos testes unitários

--- src/test/java/br/com/souza1981/bil/BilApplicationTests.java
+++ src/test/java/br/com/souza1981/bil/BilApplicationTests.java
@@ -1,13 +1,9 @@
 package br.com.souza1981.bil;
 
-import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class BilApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
